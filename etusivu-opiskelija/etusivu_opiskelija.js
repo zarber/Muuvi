50d6992f59f9a3a7f674791dfe5a4d1f24e7bc31
Enni <ennini@metropolia.fi>
2023-03-27 21:19:04 +0300

    Adding responsive top navigation

---
+++ etusivu-opiskelija/etusivu_opiskelija.js
@@ -0,0 +1,8 @@
+function topNavFunction() {
+    var x = document.getElementById("topNav");
+    if (x.className === "navigation") {
+      x.className += "responsive";
+    } else {
+      x.className = "navigation";
+    }
+  }
